4f7c3d47044aeb6a09bc3e598210ad39b7997e0b
Alyssa <alyssaw90@gmail.com>
2014-11-26 22:50:09 -0800

    Javascript problem 2 done

--- javascript_drills/problem2.js
+++ javascript_drills/problem2.js
@@ -4,6 +4,19 @@
 var artists = ["Passion Pit", "Mumford and Sons", "MGMT"];
 var albums = ["Gossamer", "Sigh no more", "Time to Pretend"];
 
+var zip = function(array1, array2) {
+	var newArray = [];
+
+	array1.forEach(function(element,index){
+		var subArray = [];
+		subArray.push(element, array2[index]);
+		newArray.push(subArray);
+	})
+	return newArray
+}
+
+console.log(zip(artists, albums));
+
 
 /*OUTPUT: [ [ 'Passion Pit', 'Gossamer' ],
   [ 'Mumford and Sons', 'Sigh no more' ],
