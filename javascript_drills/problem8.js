ce8bcca156b602f6a8df93901a8404cc02f929e9
Alyssa <alyssaw90@gmail.com>
2014-12-01 10:11:43 -0800

    js problem 8 almost done

--- javascript_drills/problem8.js
+++ javascript_drills/problem8.js
@@ -8,8 +8,28 @@ just put that letter. Hint Use a loop to iterate over
 a string. You can reference characters in a string 
 similar to an array. */
 
-pattern = "AAASSSDDDDRDDSASSDDDSSSAD" 
+var input = "AAASSSDDDDRDDSASSDDDSSSAD" 
 
+var compress = function(string){
+	var counter = 1;
+	var output = '';
+	for (var i = 0; i <= string.length; i++) {
+		if(string[i] === string[i+1]){
+			counter++;
+			// output += counter+1;
+			output += counter; 
+			// console.log(counter);
+		} else {
+			counter = 1;
+			output += string.charAt(i);
+			// console.log("No");
+		}
+	}
+	return output;
+};
 
+console.log(compress(input));
+
+// string.charAt(i);
 
 //OUTPUT: pattern = 3A3S4DR2DSA2S3D3SAD
