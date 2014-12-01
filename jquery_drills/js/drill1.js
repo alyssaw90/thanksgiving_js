9bd24b4907d6803c49fa32d4efac476cc579e0a8
Alyssa <alyssaw90@gmail.com>
2014-12-01 12:21:47 -0800

    jquery 1 and 2 updated

--- jquery_drills/js/drill1.js
+++ jquery_drills/js/drill1.js
@@ -1,12 +1,16 @@
 $(function(){
 	var ball = $('.ball');
 	ball.on("click", function() {
-		$(this).fadeOut(2000, function(){
+		$(this).fadeOut(500, function(){
 
 $(this).appendTo('div .stage').animate({
-	'left': (Math.random() * 600),
-	'top': (Math.random() * 600)
-	}).fadeIn(2000);
+	/* ($('.stage').width()-$('.shape').width())
+ 	Subtracting the shape from the stage will keep the ball
+	within the stage.
+	*/
+	'left': (Math.floor(Math.random() * ($('.stage').width()-$('.shape').width()))),
+	'top': (Math.floor(Math.random() * ($('.stage').height()-$('.shape').height())))
+	}).fadeIn(500);
 
 		})
 	})    
--- jquery_drills/js/drill2.js
+++ jquery_drills/js/drill2.js
@@ -1,33 +1,32 @@
 $(function(){
 
 
-    console.log('The dom is ready! (drill 2)');
+    // console.log('The dom is ready! (drill 2)');
 
     $(document).on('keyup',function(event){
         //output to console which key was pressed
-        var box = $(".stage"),
+        var box = $(".shape"),
         left = 37,
         up = 38,
         right = 39,
         down = 40,
         s = 83
-        event.stopPropagation();
-        event.preventDefault();
-        if (event.keyCode == left) {
+        if (event.keyCode == s) {
+        box.effect("shake");
+        }
+        else if (event.keyCode == left) {
         	box.animate({left: "-=50"}, 0);
         }
-        if (event.keyCode == up) {
+        else if (event.keyCode == up) {
         	box.animate({top: "-=50"}, 0);
         }
-        if (event.keyCode == right) {
+        else if (event.keyCode == right) {
         	box.animate({left: "+=50"}, 0);
         }
-        if (event.keyCode == down) {
+        else if (event.keyCode == down) {
         	box.animate({top: "+=50"}, 0);
         }
-        if (event.keyCode == s) {
-        	box.animate("shake");
-        }
+
         console.log('The user pressed key: ',event.which);
     });
 
